98cb3c7612eb7b92140dd20fe1e9c80d01fdf98c
utkarshbharti <utkarsh.bharti@snapwiz.com>
2019-11-09 10:20:02 +0530

    set up react-webpack-babel boilerplate

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { AppWrapper } from "./global.styled";
+const template = <AppWrapper>hello react</AppWrapper>;
+
+ReactDOM.render(template, document.getElementById("root"));
